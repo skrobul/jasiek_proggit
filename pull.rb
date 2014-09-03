ac8bd1075d7c3d7453d755be90208970284d88c3
Marek Skrobacki <skrobul@skrobul.com>
2014-09-03 22:53:45 +0100

    dodaj date

--- pull.rb
+++ pull.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/ruby
 require 'redditkit'
-cl = RedditKit::Client.new 'TWOJUSER','TWOJPASS'
+cl = RedditKit::Client.new 'TWOJLOGIN','TWOJPASS'
 npageid = ''
 
-loop do 
+loop do
  this_page_links = cl.links 'programming', after: npageid, limit: 100
  this_page_links.results.each do |link|
-   puts "#{link.title} posted by #{link.author}"
+   puts "#{link.title} posted on #{link.created_at} by #{link.author}"
  end
  npageid = this_page_links.after
  sleep 3 # make reddit admins happy (przeczytaj API rules)
